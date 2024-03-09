329ae6d526864484f237ece2452b7ed129ae252e
icecoff22 <icecoff2222@gmail.com>
2024-03-09 23:07:39 +0900

    [INIT]

---
+++ src/main/java/WebProject/TFinder/TFinderApplication.java
@@ -0,0 +1,13 @@
+package WebProject.TFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TFinderApplication.class, args);
+	}
+
+}
