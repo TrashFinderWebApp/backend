0bfdb0a74229b793ada8beb444233e4b5989702a
icecoff22 <icecoff2222@gmail.com>
2024-03-21 16:51:53 +0900

    [FIX] ID 어노테이션 수정

--- src/main/java/org/example/domain/auth/RefreshToken.java
+++ src/main/java/org/example/domain/auth/RefreshToken.java
@@ -1,8 +1,9 @@
 package org.example.domain.auth;
 
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
