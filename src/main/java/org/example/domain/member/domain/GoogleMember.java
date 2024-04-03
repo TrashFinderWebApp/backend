b9e8c881d815a37e88cb7990c56d25a7942ac297
icecoff22 <icecoff2222@gmail.com>
2024-04-04 01:14:44 +0900

    [REFACTOR] 각 소셜 로그인 유저 정보 담는 클래스 구조 수정

--- src/main/java/org/example/domain/member/domain/GoogleMember.java
+++ src/main/java/org/example/domain/member/domain/GoogleMember.java
@@ -1,8 +1,11 @@
 package org.example.domain.member.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class GoogleMember {
+
+    @JsonProperty("id")
     private String socialId;
 }
--- src/main/java/org/example/domain/member/domain/NaverMember.java
+++ src/main/java/org/example/domain/member/domain/NaverMember.java
@@ -1,8 +1,26 @@
 package org.example.domain.member.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class NaverMember {
-    private String socialId;
+
+    @JsonProperty("resultcode")
+    private String resultCode;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("response")
+    private NaverUserInfo naverUserInfo;
+
+    @Getter
+    public static class NaverUserInfo {
+        private String id;
+        private String name;
+        private String email;
+    }
+
 }
