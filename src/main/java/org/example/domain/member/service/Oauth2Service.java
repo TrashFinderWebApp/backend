87b1e3d74ec9744b24f578e669215e352786227c
icecoff22 <icecoff2222@gmail.com>
2024-04-03 21:04:12 +0900

    [FEAT] 카카오, 네이버, 구글 소셜 로그인 구현
    - 각 소셜 액세스 토큰 받기 구현
    - 액세스 토큰으로 사용자 정보 받기 구현
    	- 받는 정보는 일단 id값 뿐
    - id값 저장 후 액세스,리프레시 토큰 발급

--- src/main/java/org/example/domain/member/service/Oauth2Service.java
+++ src/main/java/org/example/domain/member/service/Oauth2Service.java
@@ -2,11 +2,15 @@ package org.example.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.domain.member.domain.GoogleMember;
 import org.example.domain.member.domain.KakaoMember;
 import org.example.domain.member.domain.Member;
+import org.example.domain.member.domain.NaverMember;
 import org.example.domain.member.domain.SocialMember;
 import org.example.domain.member.dto.response.TokenInfo;
+import org.example.domain.member.dto.token.GoogleToken;
 import org.example.domain.member.dto.token.KakaoToken;
+import org.example.domain.member.dto.token.NaverToken;
 import org.example.domain.member.repository.MemberRepository;
 import org.example.domain.member.repository.SocialMemberRepository;
 import org.example.domain.member.type.RoleType;
@@ -44,6 +48,33 @@ public class Oauth2Service {
     @Value("${oauth2.client.kakao.user-info-uri}")
     private String kakaoUserInfo;
 
+    @Value("${oauth2.client.naver.client-id}")
+    private String naverClientId;
+
+    @Value("${oauth2.client.naver.client-secret}")
+    private String naverClientSecret;
+
+    @Value("${oauth2.client.naver.token-uri}")
+    private String naverTokenUri;
+
+    @Value("${oauth2.client.naver.user-info-uri}")
+    private String naverUserInfo;
+
+    @Value("${oauth2.client.google.client-id}")
+    private String googleClientId;
+
+    @Value("${oauth2.client.google.client-secret}")
+    private String googleClientSecret;
+
+    @Value("${oauth2.client.google.redirect-uri}")
+    private String googleRedirectUri;
+
+    @Value("${oauth2.client.google.token-uri}")
+    private String googleTokenUri;
+
+    @Value("${oauth2.client.google.user-info-uri}")
+    private String googleUserInfo;
+
     public TokenInfo socialLogin(SocialType socialType, String code) {
         return switch (socialType) {
             case GOOGLE -> googleLogin(code, socialType);
@@ -61,16 +92,17 @@ public class Oauth2Service {
     }
 
     public TokenInfo naverLogin(String code, SocialType socialType) {
-        String accessToken = getGoogleAccessToken(code);
-        //String socialId = getNaverUserInfo(accessToken);
-        return null;
+        String accessToken = getNaverAccessToken(code);
+        NaverMember naverMember = getNaverUserInfo(accessToken);
+        createSocialUser(naverMember.getSocialId(), socialType);
+        return jwtProvider.createToken(naverMember.getSocialId(), socialType.toString());
     }
 
     public TokenInfo googleLogin(String code, SocialType socialType) {
-        String accessToken = getNaverAccessToken(code);
-        //getGoogleUserInfo(accessToken);
-
-        return null;
+        String accessToken = getGoogleAccessToken(code);
+        GoogleMember googleMember = getGoogleUserInfo(accessToken);
+        createSocialUser(googleMember.getSocialId(), socialType);
+        return jwtProvider.createToken(googleMember.getSocialId(), socialType.toString());
     }
 
     public String getKakaoAccessToken(String code) {
@@ -100,11 +132,51 @@ public class Oauth2Service {
 
 
     public String getNaverAccessToken(String code) {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type","code");
+        body.add("client_id", naverClientId);
+        body.add("client_secret", naverClientSecret);
+        body.add("code",code);
+        body.add("state","32142131");
+
+        HttpEntity<MultiValueMap<String, String>> tokenRequest = new HttpEntity<>(body, headers);
+        NaverToken naverToken = restTemplate.postForObject(naverTokenUri,tokenRequest, NaverToken.class);
+
+        if (naverToken == null) {
+            log.error("네이버 토큰 에러");
+
+        }
+        System.out.println(naverToken.getAccess_token());
+
+        return naverToken.getAccess_token();
     }
 
     public String getGoogleAccessToken(String code) {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type","authorization_code");
+        body.add("client_id", googleClientId);
+        body.add("client_secret", googleClientSecret);
+        body.add("code",code);
+        body.add("redirect_uri","32142131");
+
+        HttpEntity<MultiValueMap<String, String>> tokenRequest = new HttpEntity<>(body, headers);
+        GoogleToken googleToken = restTemplate.postForObject(googleTokenUri,tokenRequest, GoogleToken.class);
+
+        if (googleToken == null) {
+            log.error("구글 토큰 에러");
+
+        }
+        System.out.println(googleToken.getAccess_token());
+
+        return googleToken.getAccess_token();
     }
 
     public KakaoMember getKakaoUserInfo(String accessToken) {
@@ -121,6 +193,38 @@ public class Oauth2Service {
         //헤더 + 바디
         HttpEntity<MultiValueMap<String, String>> memberInfoRequest = new HttpEntity<>(body, headers);
         return restTemplate.postForObject(kakaoUserInfo, memberInfoRequest, KakaoMember.class);
+    }
+
+    public NaverMember getNaverUserInfo(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Authorization", "Bearer " + accessToken);
+
+        //바디 생성
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("property_keys",  "[\"response.id\"]");
+
+        //헤더 + 바디
+        HttpEntity<MultiValueMap<String, String>> memberInfoRequest = new HttpEntity<>(body, headers);
+        return restTemplate.postForObject(naverUserInfo, memberInfoRequest, NaverMember.class);
+    }
+
+    public GoogleMember getGoogleUserInfo(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Authorization", "Bearer " + accessToken);
+
+        //바디 생성
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("property_keys",  "[\"id\"]");
+
+        //헤더 + 바디
+        HttpEntity<MultiValueMap<String, String>> memberInfoRequest = new HttpEntity<>(body, headers);
+        return restTemplate.postForObject(googleUserInfo, memberInfoRequest, GoogleMember.class);
     }
 
     @Transactional
