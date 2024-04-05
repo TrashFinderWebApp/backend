02f534808ecdce2a790a7e6091086d1d7ffe44fb
icecoff22 <icecoff2222@gmail.com>
2024-04-05 13:47:48 +0900

    [FEAT] 랭킹 조회 서비스, 레포지토리 구현
    - JPQL사용하여 쿼리로 조회
    - 점수 테이블에 아무도 없으면 null 반환

--- src/main/java/org/example/domain/rank/repository/ScoreRepository.java
+++ src/main/java/org/example/domain/rank/repository/ScoreRepository.java
@@ -1,2 +1,31 @@
-package org.example.domain.rank.controller;public interface ScoreRepository {
+package org.example.domain.rank.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.example.domain.member.domain.Member;
+import org.example.domain.rank.controller.dto.RankDataResponse;
+import org.example.domain.rank.domain.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ScoreRepository extends JpaRepository<Score, Long> {
+
+    @Query(value = "SELECT m, SUM(s.eachScore) AS totalScore " +
+    "FROM Member m JOIN m.scoreList s "+
+    "GROUP BY m.id "+
+    "ORDER BY totalScore DESC")
+    List<Object[]> findMemberByScoreDesc();
+
+    default List<RankDataResponse> findMemberByScoreDescDTO() {
+        List<Object[]> result = findMemberByScoreDesc();
+        List<RankDataResponse> responseList = new ArrayList<>();
+
+        for (Object[] row : result) {
+            Member member = (Member) row[0];
+            Integer totalScore = ((Long) row[1]).intValue();
+            responseList.add(new RankDataResponse(member, totalScore));
+        }
+
+        return responseList;
+    }
 }
--- src/main/java/org/example/domain/rank/service/RankService.java
+++ src/main/java/org/example/domain/rank/service/RankService.java
@@ -1,2 +1,47 @@
-package org.example.domain.rank.service;public class RankService {
+package org.example.domain.rank.service;
+
+
+import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.member.domain.Member;
+import org.example.domain.member.repository.MemberRepository;
+import org.example.domain.member.service.MemberService;
+import org.example.domain.rank.controller.dto.RankDataResponse;
+import org.example.domain.rank.controller.dto.RankListResponse;
+import org.example.domain.rank.repository.ScoreRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RankService {
+    private final ScoreRepository scoreRepository;
+    private final MemberRepository memberRepository;
+
+    public List<RankListResponse> getScoreList() {
+        List<RankDataResponse> responseData = scoreRepository.findMemberByScoreDescDTO();
+        if (responseData == null) {
+            throw new IllegalArgumentException("resources not found");
+        }
+        return convertToRankList(responseData);
+    }
+
+    private List<RankListResponse> convertToRankList(List<RankDataResponse> responses) {
+        List<RankListResponse> listResponses = new ArrayList<>();
+
+        for (RankDataResponse row : responses) {
+            Member member = row.getMember();
+            Integer totalScore = row.getTotalScore();
+            listResponses.add(new RankListResponse(member.getId(), member.getName(), totalScore));
+        }
+
+        return listResponses;
+    }
+
+
 }
