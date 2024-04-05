0de7a915b3db030a1293eb11de0c825f97d7a538
icecoff22 <icecoff2222@gmail.com>
2024-04-05 14:21:27 +0900

    [FEAT] 멤버와 스코어 다대일 양방향 관계 설정
    - 무한 루프 조회로 인해 JsonIgnore 설정
    - 추후 리팩토링 예정

--- src/main/java/org/example/domain/member/domain/Member.java
+++ src/main/java/org/example/domain/member/domain/Member.java
@@ -1,5 +1,6 @@
 package org.example.domain.member.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,12 +8,17 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.domain.member.type.RoleType;
+import org.example.domain.rank.domain.Score;
 import org.example.global.domain.BaseTimeEntity;
 
 @Entity
@@ -38,6 +44,11 @@ public class Member extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private RoleType role;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "member")
+    @JsonIgnore
+    private List<Score> scoreList = new ArrayList<>();
 
     public Member update(String email, String name, String oauth2Id) {
         this.email = email;
