ca538ce6b678769fd73a89b1489aca35f5009224
icecoff22 <icecoff2222@gmail.com>
2024-04-08 16:06:37 +0900

    [FEAT] 회원가입/로그인 요청 유효성 검사
    - 길이, 형식 검사
    - 반환은 에러코드 및 메세지 형태

--- src/main/java/org/example/domain/member/controller/MemberController.java
+++ src/main/java/org/example/domain/member/controller/MemberController.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.member.dto.request.UserSignInRequest;
 import org.example.domain.member.dto.request.UserSignUpRequest;
@@ -16,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +36,8 @@ public class MemberController {
             @ApiResponse(responseCode = "400", description = "1. 이메일 중복 \t\n 2. 비밀번호 불일치 \t\n "
                     + "3. 이메일 혹은 비밀번호 형식이 맞지 않습니다. \t\n 4. 이메일, 비밀번호, 이름이 비어 있습니다.")
     })
-    public ResponseEntity<?> userSignUp(@Valid @RequestBody UserSignUpRequest request) {
+    public ResponseEntity<?> userSignUp(@RequestBody @Valid UserSignUpRequest request) {
+
         if (isDuplicated(request.getEmail())) {
             return new ResponseEntity<>("이메일 중복입니다. 다시 입력해주세요.", HttpStatus.BAD_REQUEST);
         }
@@ -51,7 +53,7 @@ public class MemberController {
             @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않을 때"),
             @ApiResponse(responseCode = "500", description = "서버에러")
     })
-    public ResponseEntity<?> userSignIn(@Valid @RequestBody UserSignInRequest request, HttpServletResponse response) {
+    public ResponseEntity<?> userSignIn(@RequestBody @Valid UserSignInRequest request, HttpServletResponse response) {
         try {
             TokenInfo tokenInfo = memberService.userSignIn(request);
 
--- src/main/java/org/example/domain/member/controller/MemberExceptionHandler.java
+++ src/main/java/org/example/domain/member/controller/MemberExceptionHandler.java
@@ -1,6 +1,12 @@
 package org.example.domain.member.controller;
 
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.example.global.domain.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -8,15 +14,26 @@ import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class MemberExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMemberValidationException(MethodArgumentNotValidException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<ErrorResponseDto> onConstraintValidationException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        FieldError fieldError = fieldErrors.get(fieldErrors.size()-1);
+        //String fieldName = fieldError.getField();
+        //Object rejectedValue = fieldError.getRejectedValue();
+
+        return new ResponseEntity<>(new ErrorResponseDto(fieldError.getDefaultMessage()),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleMemberValidationException2(Exception e) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-        String message = Optional.ofNullable(e.getFieldError())
-                .map(FieldError::getDefaultMessage)
-                .orElse("");
-        return new ResponseEntity<>(message, httpStatus);
+
+        return new ResponseEntity<>(e.getMessage(), httpStatus);
     }
 }
