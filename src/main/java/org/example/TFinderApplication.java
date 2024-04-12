eecb06fb2407cbd63a31416710058783eb2f75e4
sangpyeong <miasp1105@naver.com>
2024-04-12 23:27:58 +0900

    [FIX] api/trashcan/ cors에러 임시 해결 방법2

--- src/main/java/org/example/TFinderApplication.java
+++ src/main/java/org/example/TFinderApplication.java
@@ -3,11 +3,23 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TFinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(TFinderApplication.class, args);
 
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://front-server.com");
+            }
+        };
     }
 }
