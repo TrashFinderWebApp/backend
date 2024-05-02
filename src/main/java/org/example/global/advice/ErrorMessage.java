2ec39d1365ecb026fda5fade0c4e7069ef82264e
Icecoff22 <icecoff2222@gmail.com>
2024-05-02 18:35:23 +0900

    [FIX] pr중 conflict 해결

---
+++ src/main/java/org/example/global/advice/ErrorMessage.java
@@ -0,0 +1,13 @@
+package org.example.global.advice;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErrorMessage {
+    @JsonProperty("message")
+    private String message;
+}
